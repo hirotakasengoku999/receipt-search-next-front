b1dedbdf67f25934e22b10fa802150a915d86fa8
hirotakasengoku <hirotakasengoku@gmail.com>
2022-10-22 17:32:17 +0900

    edit detail add padding

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Layout from '../components/Layout'
+import { TextInput, Label, Button } from 'flowbite-react'
 
 export default function Home() {
   return (
     <Layout title='Home'>
-      <p className='text-4xl'>Hello NextJs</p>
-    </Layout>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="password1"
+            value="Your password"
+          />
+        </div>
+        <TextInput
+          id="password1"
+          type="password"
+          required={true}
+        />
+      </div>
+      <Button type="submit">
+        Submit
+      </Button>
+      </Layout>
   )
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -49,68 +49,70 @@ export default function Post({ staticPost, receiptCode }) {
 
     return (
         <Layout title={post.ReceiptName}>
-            <Table>
-                <Table.Body className="divide-y">
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            診療行為コード
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.ReceiptCode}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            診療行為名称
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.ReceiptName}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            点数
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.Points}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            入外
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.InOutDiv}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            区分
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.Category}
-                        </Table.Cell>
-                    </Table.Row>
-                </Table.Body>
-            </Table>
-            <Link href="/receipt-search">
-                <div className="flex cursor-pointer mt-12">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6 mr-3">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" 
-                            />
-                    </svg>
-                    <span>戻る</span>
-                </div>
-            </Link>
+            <div className="p-5">
+                <Table>
+                    <Table.Body className="divide-y">
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                診療行為コード
+                            </Table.Cell>
+                            <Table.Cell>
+                                {post.ReceiptCode}
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                診療行為名称
+                            </Table.Cell>
+                            <Table.Cell>
+                                {post.ReceiptName}
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                点数
+                            </Table.Cell>
+                            <Table.Cell>
+                                {post.Points}
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                入外
+                            </Table.Cell>
+                            <Table.Cell>
+                                {post.InOutDiv}
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="bg-gray-100 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                区分
+                            </Table.Cell>
+                            <Table.Cell>
+                                {post.Category}
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+                <Link href="/receipt-search">
+                    <div className="flex cursor-pointer mt-12">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6 mr-3">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" 
+                                />
+                        </svg>
+                        <span>戻る</span>
+                    </div>
+                </Link>
+            </div>
         </Layout>
     );
 }
